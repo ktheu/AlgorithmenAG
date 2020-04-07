6a6b587c8c2fd2aba5faaa5dbb2f539be9a43fa7
ktheu <khtheuer@t-online.de>
2020-04-07 11:11:37 +0200

    luftballons

--- luftballons/Luftballons.py
+++ luftballons/Luftballons.py
@@ -1,167 +1,98 @@
-# -*- coding: utf-8 -*-
-import itertools
-datapath = 'Bwinf/luftballons/myLoesung/'
-datapath = ''
-
-
-def init_data(nr):
-    '''
-    nr: int, Nummer der Eingabedata
-    k: laenge der lvm k >= 1
-    returns: tupel lvm, ballons
-        lvm: Liste mit der SchalenBelegung
-        ballons: Liste mit den Ballons, die nachgefüllt werden.
-    
-    '''
-    with open(datapath+'luftballons'+str(nr)+'.txt') as f:
-       ballons = f.read().splitlines()
-    ballons = [int(k) for k in ballons]
-    lvm = ballons[:10]
-    ballons = ballons[10:]
-    return lvm,ballons
-
+from itertools import combinations
+from collections import deque
 
 def bestwahl(lvm,target):
     '''
-    returns: Tupel aus Liste und int
-    Die Liste ist eine Liste der Indizes, mit der man die Summe target
-    erreichen kann. Wenn target nicht genau erreicht wird, dann
-    soll die Summe möglichst wenig über target sein. Wenn auch
-    das nicht geht, soll die leere Liste zurückgegeben werden.
-    Die Zahl ist die erreichte Summe (0 bei leerer Liste, wenn 
-    alles gut geht target, sonst halt drüber).
-    '''
-    faecher = list(range(len(lvm)))  # Liste mit den Fächerindizes
-    bestwahl = ()
-    bestsumme = 0
-    for i in range(len(lvm)):
-        for x in itertools.combinations(faecher,i):
-            summe = sum(lvm[k] for k in x)
-            if summe == target:
-                return list(x),target
-            else:
-                if (bestsumme == 0 and summe > target) or (target < summe < bestsumme):
-                    bestsumme = summe
-                    bestwahl = x
-    return list(bestwahl), bestsumme    
-            
+    returns: index des Elements, das für target gewählt werden soll
 
+    Falls mit einer Kombination das target genau getroffen wird, wird er
+    Index des größten Elements der Kombination zurückgegeben.
 
-def kippen_und_nachfuellen(fachnr,lvm,ballons):
-    '''
-    Aus dem Fach mit nr fachNr werden die Ballons in die schale gekippt,
-    Die Schale wird dann mit der nächsten Zahl aus der Liste ballons
-    aufgefüllt, falls da noch was drin ist. Sonst wird sie auf 0 gesetzt.
-    fachnr, schale: int
-    lvm, ballons: list
-    returns: anzahl Ballons, die in die Schale gekippt wurden,
-       ändert lvm und ballons
-    '''
-    anzahl_ballons = lvm[fachnr]
-    if ballons: 
-        lvm[fachnr] = ballons.pop(0)
-    else:
-        lvm[fachnr] = 0
-    return anzahl_ballons
+    Falls das target nicht genau getroffen wird, wird der Index des kleinesten Elements der
+    Kombination zurückgegeben, die möglichst wenig über der 20 ist.
+
+    Falls mit allen Zahlen das target nicht ereicht wird, wird der Index mit der kleinsten Zahl > 0
+    zurückgegeben.
     
+    '''
 
-def schale_fuellen(lvm,ballons):
-    target = 20
-    schale = 0
+    if sum(lvm) < target:             # target kann nicht erreicht werden
+        return lvm.index(min(lvm))    
 
-    while schale < 20:
-        print('lvm:',*lvm,'fuellliste:',*ballons)
-        wahl, _ = bestwahl(lvm,target)
-        if len(wahl) == 0:
-            return 0
-        fachnr = wahl[0]
-        anzahl = kippen_und_nachfuellen(fachnr,lvm,ballons)
-        target = target - anzahl
-        schale += anzahl
-        print('Kippe {} Ballons aus Fach {} aus: {} in der Schale'.format(anzahl,fachnr+1,schale))
-    print('lvm:',*lvm,'fuellliste:',*ballons)    
-    return schale
+    bestwahl = None
+    bestsumme = -1
+
+    for i in range(1,11):
+        for x in combinations(lvm,i):
+            summe = sum(x)
+            if summe == target:             # target wird genau erreicht
+                return lvm.index(max(x))  
+            if 20 < summe < bestsumme:
+                bestsumme = summe
+                bestwahl = x
+    
+    if bestsumme == -1:                     # target wird nicht erreicht 
+        return lvm.index(min([x for x in lvm if x > 0])) 
+    else:
+        return lvm.index(min(bestwahl))     # wert über target wurde erreicht
+             
 
 
-import unittest
-class MyClassTest(unittest.TestCase):
-    def test_init_data(self):
-        lvm, ballons = init_data(1)
-        self.assertListEqual(lvm,[5,3,8,3,6,2,8,4,2,2])
-        self.assertListEqual(ballons,[9,1,3,11,6,4,7,5])
-        wahl, summe = bestwahl(lvm,20)
-        self.assertListEqual(wahl,[2,6,7])
-        self.assertEqual(summe,20)
-        fachnr = wahl[0]
-        self.assertEqual(fachnr,2)
-        anzahl = kippen_und_nachfuellen(fachnr,lvm,ballons)
-        self.assertEqual(anzahl,8)
 
-        
-#if __name__ == '__main__':
- #   unittest.main()     
+'''
+solange noch ballons vorhanden sind:
+    wähle den besten index aus den Fächern aus
+    leere das fach in die ausgabe
+    fülle nach
+    wenn ausgaben >= 20: fülle ab
+'''
+
+
+'''
+target = 20
+schale = 0
+verpackt = 0
+
+ballons = ballons + 10*[0]    # damit auch die letzten noch abgefüllt werden können
+
+while ballons oder schale+lvm > target:
+    i = bestwahl(lvm,target)
+    anzahl = lmv[i]
+    lvm[i] = ballon.popleft()
+    target -= anzahl
+    
+    schale += anzahl
+    verpackt += anzahl
+    if schale >=20:
+        verpacke schale Ballons
+        target = 20
+        schale = 0
+'''
+
+f = open('./beispieldaten/luftballons1.txt')
+a = [int(x.rstrip('\n')) for x in f.readlines()]
+lvm = a[:10]
+ballons = a[10:]  
+ballons = deque(ballons)          # Schlange
 
 schale = 0    
 verpackt = 0 
-lvm, ballons = init_data(3)
-
-while ballons or sum(lvm)+schale > 20:
-    schale = schale_fuellen(lvm,ballons)
-    verpackt+=schale
-    if schale == 0:
-        print('Keine weitere Füllung möglich')
-        break
-    print('Bisher verpackt:',verpackt)
+target = 20
+while ballons or schale + sum(lvm) >= 20:  
+    i = bestwahl(lvm,target)
+    anzahl = lvm[i]
+    if (ballons):
+        lvm[i] = ballons.popleft()
+    else:
+        lvm[i] = 0
+    target -= anzahl
+    schale += anzahl
+    verpackt += anzahl
+    if schale >= 20:
+        print("Verpacke {} Ballons".format(schale))
+        target = 20
+        schale = 0
 
 print('Es wurden insgesamt {} Ballons verpackt. Der Rest ist {}'.format(verpackt,sum(lvm)))
     
 
-#fachnr = wahl[0]
-
-#print(schale,'#',*lvm,'#',*ballons)
-#fill_lvm(a)
-#get_faecher_indizes(a)
-#print(*lvm)
-#print(*ballons)
-
-
-# print(lvm)
-# tab = [[0]*10 for i in range(21)]
-#
-# '''
-# tab[s][i] genau dann wenn sich die Summe s mit den Fächern 0..i erreichen lässt
-# '''
-# def ersteEins(tab,zeile):
-#
-#    for k in range(len(tab[0])):
-#        if tab[zeile][k] == 1:
-#            return k
-#    return -1
-#
-#
-# i = 0
-# anzahl = lvm[i]
-# print("im Fach {} sind {} Luftballone".format(i,anzahl))
-# tab[anzahl][i]= 1
-#
-#
-# for i in range(1,10):
-#    anzahl = lvm[i]
-#    print("im Fach {} sind {} Luftballone".format(i,anzahl))
-#    tab[anzahl][i]= 1
-#    for j in range(21):
-#        if tab[j][i-1] == 1 and j+anzahl <= 20:
-#            tab[j+anzahl][i] = 1
-#            tab[j][i] = 1
-#
-#
-# header = [i for i in range(10)]
-# print(" ".rjust(2),':',*header)
-#
-# for i in range(21):
-#    print(str(i).rjust(2),':',*tab[i])
-#
-#
-#
-#
-# print(faecher)
--- luftballons/luftballons.md
+++ luftballons/luftballons.md
@@ -13,4 +13,14 @@
 #### Python
 
 
+#### Ergebnisse
 
+```
+1: Es wurden insgesamt 80 Ballons verpackt. Der Rest ist 9
+2: Es wurden insgesamt 80 Ballons verpackt. Der Rest ist 0
+3: Es wurden insgesamt 220 Ballons verpackt. Der Rest ist 19   falsch
+4: Es wurden insgesamt 440 Ballons verpackt. Der Rest ist 18
+5: Es wurden insgesamt 420 Ballons verpackt. Der Rest ist 15
+6: Es wurden insgesamt 420 Ballons verpackt. Der Rest ist 15
+7: Es wurden insgesamt 0 Ballons verpackt. Der Rest ist 10     falsch
+```
